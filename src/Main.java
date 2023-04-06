9df5686cf4326a182f79e5aa9c1c9f655c9c1b3f
Netology <orjuice@mail.ru>
2023-04-06 20:36:39 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 15000;
+        int  bonusPerThisAmount= 20;
+        System.out.println( "Начисленные мили: " + price / bonusPerThisAmount);
+    }
+}
+
